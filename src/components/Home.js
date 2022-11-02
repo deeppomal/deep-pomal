2bd8ac072dcbe828c23fe2325b09bd774c6278a8
hitesh0100 <dpomal3268@conestogac.on.ca>
2022-11-02 17:52:15 -0400

    initial setup

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Footer } from './Footer'
+import { Header } from './Header'
+import { Main } from './Main'
+
+export const Home = () => {
+  return (
+    <div>
+        <Header />
+        <Main />
+        <Footer />
+    </div>
+  )
+}
